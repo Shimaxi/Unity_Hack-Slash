b5e948d0a2e0b98037811837ece35cdde1be2bbd
Shimaxi <mk.shk10017@gmail.com>
2023-08-18 20:49:36 +0900

    敵と戦えるように

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyController : MonoBehaviour
 {
     //•Ï”éŒ¾
     public EnemyModel m_EnemyModel; //CardModelŒ^‚Å‘‚©‚ê‚½ƒJ[ƒh‚Ìî•ñ
+    public Image m_EnemyImage;
     public void Init(int enemyID)
     {
         m_EnemyModel = new EnemyModel(enemyID); //CardModelŒ^‚Ìî•ñ‚ğ•Ï”m_CardModel‚É“ü‚ê‚é
+        m_EnemyImage.sprite = m_EnemyModel.m_EnemyImage;
     }
 }
